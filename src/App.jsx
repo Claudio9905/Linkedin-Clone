70d06b451e513c1f89c8c9f7d157ce651d036db2
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-09-01 17:01:35 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,16 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import Profile from "./components/Profile";
-<<<<<<< HEAD
-import NewsSection from './components/NewsSection';
-function App() {
-  return (
-    <>
-      <MyNavbar />
-      <Profile />
-      <NewsSection />
-=======
-import MyFooter from "./components/footer/MyFooter";
-
+import NewsSection from "./components/NewsSection";
 function App() {
   return (
     <>
       <Container fluid className="sfondoContainer">
         <MyNavbar />
+
         <Profile />
         <MyFooter />
       </Container>
->>>>>>> 298bee45225f573e94d7899e478b27858a10cc3a
     </>
   );
 }
