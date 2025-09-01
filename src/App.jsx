a33ef7bc102560c4a66a8c61e007d88fa894e864
Claudio9905 <cloudpost10@gmail.com>
2025-09-01 10:33:43 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MyNavbar from "./components/MyNavbar";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+      <Profile />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Profile = () => {
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Profile;
