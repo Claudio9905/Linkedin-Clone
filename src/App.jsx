7dbc1ebf6a6ba52b1979d11c4701afbf9db6e9a5
Claudio9905 <cloudpost10@gmail.com>
2025-09-01 15:45:44 +0200

    MyBranch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import MyNavbar from "./components/MyNavbar";
 import Profile from "./components/Profile";
 import MyFooter from "./components/footer/MyFooter";
 
 function App() {
   return (
     <>
-      <MyNavbar />
       <Profile />
       <MyFooter />
     </>
