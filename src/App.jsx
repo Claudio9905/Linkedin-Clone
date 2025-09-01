10203534e90b25f1362a2ab993a9d9c056769b45
Pasquale-Lino <pasquale.alberico94@gmail.com>
2025-09-01 15:28:49 +0200

    pasquale

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import Profile from "./components/Profile";
-
+import NewsSection from './components/NewsSection';
 function App() {
   return (
     <>
       <MyNavbar />
       <Profile />
+      <NewsSection />
     </>
   );
 }
