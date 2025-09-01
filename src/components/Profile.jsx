769d43e27666d8e7a470245b35560fbcd9333200
Claudio9905 <cloudpost10@gmail.com>
2025-09-01 15:29:14 +0200

    MyBranch

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,17 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import MainProfile from "./MainProfile";
 
 const Profile = () => {
   return (
     <>
       <Container fluid>
         <Row>
-          <Col></Col>
-          <Col></Col>
+          <Col xs={12} md={7}>
+            <MainProfile />
+          </Col>
+          <Col xs={12} md={5}></Col>
         </Row>
       </Container>
     </>
