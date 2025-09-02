4c90e2cc2602a31ec25a36b95bee06853d7ced67
Claudio9905 <cloudpost10@gmail.com>
2025-09-02 10:33:47 +0200

    MyBranch

---
+++ src/redux/actions/index.js
@@ -0,0 +1,31 @@
+export const GET_PROFILE = "GET_PROFILE";
+
+const endpoint = "https://striveschool-api.herokuapp.com/api/profile/me";
+
+export const getMeProfileAction = () => {
+  return (dispatch) => {
+    fetch(endpoint, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OGI1NTM5ZWQyOWE0OTAwMTUxZjIwODciLCJpYXQiOjE3NTY3MTM4ODYsImV4cCI6MTc1NzkyMzQ4Nn0.g_ltzuAOM5iLWXZ42EnQunI2ClCn8d1JaOjtHmS1rNA",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore nel recupero dei dati");
+        }
+      })
+      .then((resData) => {
+        console.log(resData);
+        dispatch({
+          type: GET_PROFILE,
+          payload: resData,
+        });
+      })
+      .catch((err) => {
+        console.log("ERRROR: ", err);
+      });
+  };
+};
