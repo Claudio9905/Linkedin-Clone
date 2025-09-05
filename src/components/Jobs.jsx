b7b2b49f051a720cd70136705d0c960baba472e2
Claudio9905 <cloudpost10@gmail.com>
2025-09-05 11:15:34 +0200

    MyBranch

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,13 +1,21 @@
-import { useEffect } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import {
+  Col,
+  Container,
+  Form,
+  FormControl,
+  FormGroup,
+  Row,
+} from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import { useDispatch, useSelector } from "react-redux";
-import { getJobsAction } from "../redux/actions";
+import { getJobsAction, getSingleJobAction } from "../redux/actions";
 import Spinner from "react-bootstrap/Spinner";
 
 const Jobs = () => {
   const dispatch = useDispatch();
-  // const dispatch2 = useDispatch();
+  const dispatch2 = useDispatch();
+  const [title, setTitle] = useState("");
   // const dispatch3 = useDispatch();
   // const dispatch4 = useDispatch();
 
@@ -16,9 +24,10 @@ const Jobs = () => {
     dispatch(getJobsAction());
   }, [dispatch]);
 
-  // useEffect(() => {
-  //   dispatch2(getSingleJobAction("engineer"));
-  // }, []);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    dispatch2(getSingleJobAction(title));
+  };
 
   // useEffect(() => {
   //   dispatch3(getJobCompanyAction("CYOS Solution"));
@@ -27,6 +36,13 @@ const Jobs = () => {
   // useEffect(() => {
   //   dispatch4(getJobCategoryAction("All others"));
   // });
+  const singleJob = useSelector((state) => {
+    return state.singleJob.singleJob;
+  });
+
+  const foundJob = useSelector((state) => {
+    return state.singleJob.found;
+  });
 
   const getJobs = useSelector((state) => {
     return state.jobs.jobs;
@@ -41,69 +57,158 @@ const Jobs = () => {
   return (
     <>
       <Container>
-        <Row className="flex-column g-4" id="row-job-search">
-          {isloading ? (
-            <Col xs={12} className="d-flex justify-content-center">
-              <Spinner animation="grow" variant="primary"></Spinner>
-            </Col>
-          ) : (
-            getJobs.slice(0, 30).map((job, i) => {
-              return (
-                <Col
-                  key={i}
-                  xs={12}
-                  md={12}
-                  lg={12}
-                  className=" border border-1 border-light rounded-4 p-3 col-job animation-start"
-                >
-                  <Card className="edit-job">
-                    <Card.Body
-                      className="d-flex flex-row justify-content-between
-                  "
-                    >
-                      <div className=" w-50">
-                        <Card.Title className="fs-3">{job.title}</Card.Title>
-                        <Card.Text>
-                          <span className=" text-decoration-underline">
-                            Company Name
-                          </span>{" "}
-                          : {job.company_name}
-                        </Card.Text>
-                        <Card.Text>
-                          <span className=" text-decoration-underline">
-                            Category
-                          </span>{" "}
-                          : {job.category}
-                        </Card.Text>
-                        <Card.Text>
-                          <span className=" text-decoration-underline">
-                            Job Type
-                          </span>{" "}
-                          : {job.job_type}
-                        </Card.Text>
-                        <a href={job.url} className=" text-decoration-none">
-                          {" "}
-                          {job.url}
-                        </a>
-                        <Card.Text>
-                          <span className=" text-decoration-underline">
-                            Candidate required location
-                          </span>
-                          : {job.candidate_required_location}
-                        </Card.Text>
-                      </div>
-                      <div>
-                        <Card.Text className="date-job">
-                          {job.publication_date.slice(2, 10)}
-                        </Card.Text>
-                      </div>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              );
-            })
-          )}
+        <Row>
+          <Col
+            xs={12}
+            md={12}
+            lg={12}
+            className=" border border-1 border-light rounded-4 p-3 col-job animation-start"
+          >
+            <Form onSubmit={handleSearch}>
+              <FormGroup>
+                <FormControl
+                  type="search"
+                  placeholder="Trova un annuncio"
+                  value={title}
+                  onChange={(e) => {
+                    setTitle(e.target.value);
+                  }}
+                ></FormControl>
+              </FormGroup>
+            </Form>
+          </Col>
         </Row>
+
+        {foundJob ? (
+          <Row className="flex-column g-4" id="row-job-search">
+            {
+              <Col xs={12} className="d-flex justify-content-center">
+                <Spinner animation="grow" variant="primary"></Spinner>
+              </Col>
+            }
+            {foundJob &&
+              title &&
+              singleJob.slice(0, 50).map((job, i) => {
+                return (
+                  <Col
+                    key={i}
+                    xs={12}
+                    md={12}
+                    lg={12}
+                    className=" border border-1 border-light rounded-4 p-3 col-job animation-start"
+                  >
+                    <Card className="edit-job">
+                      <Card.Body
+                        className="d-flex flex-row justify-content-between
+                  "
+                      >
+                        <div className=" w-50">
+                          <Card.Title className="fs-3">{job.title}</Card.Title>
+                          <Card.Text>
+                            <span className=" text-decoration-underline">
+                              Company Name
+                            </span>{" "}
+                            : {job.company_name}
+                          </Card.Text>
+                          <Card.Text>
+                            <span className=" text-decoration-underline">
+                              Category
+                            </span>{" "}
+                            : {job.category}
+                          </Card.Text>
+                          <Card.Text>
+                            <span className=" text-decoration-underline">
+                              Job Type
+                            </span>{" "}
+                            : {job.job_type}
+                          </Card.Text>
+                          <a href={job.url} className=" text-decoration-none">
+                            {" "}
+                            {job.url}
+                          </a>
+                          <Card.Text>
+                            <span className=" text-decoration-underline">
+                              Candidate required location
+                            </span>
+                            : {job.candidate_required_location}
+                          </Card.Text>
+                        </div>
+                        <div>
+                          <Card.Text className="date-job">
+                            {job.publication_date.slice(2, 10)}
+                          </Card.Text>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+          </Row>
+        ) : (
+          <Row className="flex-column g-4" id="row-job-search">
+            {isloading ? (
+              <Col xs={12} className="d-flex justify-content-center">
+                <Spinner animation="grow" variant="primary"></Spinner>
+              </Col>
+            ) : (
+              getJobs.slice(0, 30).map((job, i) => {
+                return (
+                  <Col
+                    key={i}
+                    xs={12}
+                    md={12}
+                    lg={12}
+                    className=" border border-1 border-light rounded-4 p-3 col-job animation-start"
+                  >
+                    <Card className="edit-job">
+                      <Card.Body
+                        className="d-flex flex-row justify-content-between
+                  "
+                      >
+                        <div className=" w-50">
+                          <Card.Title className="fs-3">{job.title}</Card.Title>
+                          <Card.Text>
+                            <span className=" text-decoration-underline">
+                              Company Name
+                            </span>{" "}
+                            : {job.company_name}
+                          </Card.Text>
+                          <Card.Text>
+                            <span className=" text-decoration-underline">
+                              Category
+                            </span>{" "}
+                            : {job.category}
+                          </Card.Text>
+                          <Card.Text>
+                            <span className=" text-decoration-underline">
+                              Job Type
+                            </span>{" "}
+                            : {job.job_type}
+                          </Card.Text>
+                          <a href={job.url} className=" text-decoration-none">
+                            {" "}
+                            {job.url}
+                          </a>
+                          <Card.Text>
+                            <span className=" text-decoration-underline">
+                              Candidate required location
+                            </span>
+                            : {job.candidate_required_location}
+                          </Card.Text>
+                        </div>
+                        <div>
+                          <Card.Text className="date-job">
+                            {job.publication_date.slice(2, 10)}
+                          </Card.Text>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })
+            )}
+          </Row>
+        )}
       </Container>
     </>
   );
